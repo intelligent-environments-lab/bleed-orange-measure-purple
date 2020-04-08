fc47c3e27d46cfa0ddea030617795dec4189ceb0
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2020-04-07 23:00:06 -0500

    Altered file to handle vertical data
    File now works with new orientation of data in csv files. Also, found and fixed an error where pm2.5 values included pm10.

--- aps_pm_datafile.py
+++ aps_pm_datafile.py
@@ -15,38 +15,40 @@ class APSPMfile(commonfile):
         raw_data = pd.read_csv(pmfile,index_col=False)
         
         def _isolate_pm(data):
-            start = data.index[data.iloc[:,1] == 'Date'][0]
-            data = data.iloc[start:,:]
+            start_row_index = data.index[data.iloc[:,1] == 'Date'][0]
+            data = data.iloc[start_row_index:,:]
             data.rename(columns = data.iloc[0], inplace = True)
             data = data.iloc[1:,:]
             data.reset_index(drop = True,inplace = True)
             return data
         
-        pm25data = _isolate_pm(raw_data)
+        self.data = _isolate_pm(raw_data)
         
-        self.pm25data = self._parse_pm25(pm25data)
+        self.pm25_data = self._parse_pm25(self.data)
         self.frequency = None
 
 
     def _parse_pm25(self,data):
-        temparray = []
+        array_1 = []
         timearray = []
-        for col in data.iloc[:,1:].iteritems():
-            col = np.array(col[1])    #Access series in tuple (technical detail), convert to np array
-            col_pm = col[3:] #Access pm values part of series
-            timestamp =  col[0]+' '+col[1]
+        for row in data.iterrows():
+            row = np.array(row[1])    #Access series in tuple (technical detail), convert to np array
+            col_names = data.columns
+            row_pm = row[col_names.get_loc('<0.523'):col_names.get_loc(2.642)] #Access pm values part of series
+            timestamp =  row[1]+' '+row[2]
             timearray.append(timestamp)
-            temparray.append(np.sum(col_pm.astype(float))*1000)
+            
+            #Sum mass to get pm 2.5 mass
+            array_1.append(np.sum(row_pm.astype(float))*1000)
     
-            # print(col)
         timearray = np.array(timearray)
         # timearray = self._str2date(np.array(timearray))
-        timearray = super()._str2date(np.array(timearray),'%m/%d/%y %H:%M:%S')
+        timearray = super()._str2date(np.array(timearray),'%m/%d/%Y %H:%M:%S')
 
-        temparray= pd.DataFrame([timearray,temparray]).transpose()
-        temparray.columns = ['time','pmdata']
-        temparray = temparray.set_index('time').astype(float)
-        return temparray
+        array_1= pd.DataFrame([timearray,array_1]).transpose()
+        array_1.columns = ['time','pmdata']
+        array_1 = array_1.set_index('time').astype(float)
+        return array_1
     
 
     @property
@@ -56,10 +58,10 @@ class APSPMfile(commonfile):
     def pm(self): return self.pm25data.astype(float)['pmdata']
     
     @property
-    def hourly_time(self): return super().resample(self.pm25data.astype(float),'time','H')
+    def hourly_time(self): return super().resample(self.pm25_data.astype(float),'time','H')
     
     @property 
-    def hourly_pm(self): return super().resample(self.pm25data.astype(float),'pmdata','H')
+    def hourly_pm(self): return super().resample(self.pm25_data.astype(float),'pmdata','H')
     
 if __name__ == "__main__":
     PmFile = APSPMfile('input\\test3\\Test_C_0304.csv')
