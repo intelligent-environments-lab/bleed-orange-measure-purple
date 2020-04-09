6a87514b6a3026af51039f9f2610d1adbbaa74c4
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2020-04-08 21:13:53 -0500

    Autofmt xdates

--- pa_covid_runner.py
+++ pa_covid_runner.py
@@ -8,6 +8,7 @@ Created on Wed Apr  8 17:12:21 2020
 from pa_datafile import PAfile
 import matplotlib.pyplot as plt
 from matplotlib.dates import date2num
+import matplotlib.dates as mdates
 import numpy as np
 
 import os
@@ -22,17 +23,22 @@ for filename in os.listdir(cwd+r'\input\pa_covid'):
         PAfiles.append(PAfile('input\\pa_covid\\'+filename))
         
 fig = plt.figure(figsize=(20,10))
+ax = fig.add_subplot(1,1,1)  
 time =[]
 for file in PAfiles:
     # file.set_frequency('H')
     plt.plot_date(file.hourly_time,file.hourly_pm,'.',xdate=True,label=file.sensorname)
     plt.legend()
     time = file.time
+
+ax.xaxis.set_major_locator(mdates.DayLocator(interval=1)) 
+ax.grid()
 plt.ylim(0,70)
+fig.autofmt_xdate()
 plt.title('Hourly PM 2.5 Values from UT PurpleAirs for Mar 1 to Apr 8')
 plt.ylabel('PM 2.5 (ug/m3)')
 plt.xlabel('Time')
-fig.savefig('output//march_ut_pa_hourly.svg')
+fig.savefig('output//march_ut_pa_hourly_pm.svg')
 
 fig = plt.figure(figsize=(20,10))
 time =[]  
