15c394337abb28d04cdb46a1bdbcd2c6f3feeed2
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2020-04-10 00:09:29 -0500

    Update pa_covid_runner.py

--- pa_covid_runner.py
+++ pa_covid_runner.py
@@ -17,16 +17,19 @@ USE_CACHE = True
 CACHE = 'PAfiles_cache.pkl'
 
 # Using caching for improved performance
-if not path.exists(CACHE) or not USE_CACHE:
-    print('Importing data from csv...', flush=True, end="")
-    PAfiles = PAfile.import_pa_files(os.getcwd(), 'input\\pa_covid')
-    print('Done', flush=True)
-    pkl.dump(PAfiles, open(CACHE, 'wb'))
-else:
-    print('Loading data from cache...', flush=True, end="")
-    PAfiles = pkl.load(open(CACHE, 'rb'))
-    print('Done', flush=True)
+def import_pa():
+    if not path.exists(CACHE) or not USE_CACHE:
+        print('Importing data from csv...', flush=True, end="")
+        PAfiles = PAfile.import_pa_files(os.getcwd(), 'input\\pa_covid')
+        print('Done', flush=True)
+        pkl.dump(PAfiles, open(CACHE, 'wb'))
+    else:
+        print('Loading data from cache...', flush=True, end="")
+        PAfiles = pkl.load(open(CACHE, 'rb'))
+        print('Done', flush=True)
+    return PAfiles
 
+PAfiles = import_pa()
 fig = plt.figure(figsize=(20, 10))
 ax = fig.add_subplot(1, 1, 1)
 for file in PAfiles:
@@ -34,6 +37,7 @@ for file in PAfiles:
     plt.plot_date(file.time,
                   file.pm25,
                   '.', xdate=True, label=file.sensorname)
+
 ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
 ax.grid()
 plt.ylim(0, 70)
