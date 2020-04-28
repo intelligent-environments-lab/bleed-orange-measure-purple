9e5dc22cf05a376b49394735bd9c0b2ec8a27022
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2020-04-28 12:22:20 -0500

    Update pa_covid_runner.py
    Changed sample frequency while creating plots

--- pa_covid_runner.py
+++ pa_covid_runner.py
@@ -34,7 +34,7 @@ def plot_purpleair_pm(fig, files, param='PM2.5_ATM_ug/m3'):
     for file in files:
         if file.hourly[param] is not None:
             file.frequency = None
-            filtered = remove_outlier(file[:], param).resample('D').mean()
+            filtered = remove_outlier(file[:], param).resample('H').mean()
             # values = file.raw[param]
             values = filtered[param]
             time = values.index.to_pydatetime()
@@ -54,10 +54,11 @@ def plot_avg_param(fig, param='Temperature_F', second_y=False, r=2, c=1):
                                mode='lines',
                                name=param), row=r, col=c, secondary_y=second_y)
 
-def plot_avg_pm(fig, param='PM2.5_ATM_ug/m3', second_y=False, r=1, c=1):
+
+def plot_avg_pm(fig, param='PM2.5_ATM_ug/m3', second_y=False, r=1, c=1, freq=None):
     
     #A list of series with PM data
-    combined_data = [remove_outlier(file[:], param).resample('H').mean()[param].rename(file.sensorname) 
+    combined_data = [remove_outlier(file[:], param).resample(freq).mean()[param].rename(file.sensorname) 
                      for file in pa_files if file[param] is not None]
     combined_data =  pd.concat(combined_data, axis=1) #columns = sensors, rows = pm values
     avg = combined_data.mean(axis=1)  #average all sensors
@@ -65,40 +66,56 @@ def plot_avg_pm(fig, param='PM2.5_ATM_ug/m3', second_y=False, r=1, c=1):
     time = values.index.to_pydatetime()
     fig.add_trace(go.Scattergl(x=time, y=values,
                                mode='lines',
-                               name=param), row=r, col=c, secondary_y=second_y)
+                               name='PurpleAir PM2.5'), row=r, col=c, secondary_y=second_y)
 
-def label_plot2(fig):
-    fig.update_layout(
-        yaxis2=dict(title="Temperature(F)", side='left', anchor='x2'),
-        yaxis3=dict(title="Relative humidity(%)", side='right', anchor='x2')
-        )
-
+def plot_tceq_trh(fig, freq=None):
+    tceq_trh = pd.read_csv('data/ytd/tceq_trh.csv')
+    tceq_trh['Time']=pd.to_datetime(tceq_trh['Time'], format='%Y-%m-%d %H:%M:%S')
+    tceq_trh = tceq_trh.set_index('Time')
+    tceq_trh = tceq_trh.resample(freq).mean()
+    
+    fig.add_trace(go.Scattergl(x=tceq_trh.index, y=tceq_trh['Temperature(F)'],
+                               mode='lines', name='TCEQ Temperature'), row=2, col=1, secondary_y=False)
+    fig.add_trace(go.Scattergl(x=tceq_trh.index, y=tceq_trh['Relative Humidity(%)'],
+                               mode='lines', name='TCEQ Relative Humidity'), row=2, col=1, secondary_y=True)
+    
 def label_plot(fig):
     fig.update_layout(
-        title='UT PurpleAir',
-        yaxis_title='PM 2.5(ug/m3)'#,
-        #xaxis = dict(rangeslider=dict(visible=True))
+        yaxis2=dict(title="Temperature(F)", side='left', anchor='x2'),
+        yaxis3=dict(title="Relative humidity(%)", side='right', anchor='x2'),
+        title='UT PurpleAir Hourly Averages',
+        yaxis_title='PM 2.5(ug/m3)'
         )
-    plot(fig, filename='plots/Mar1-Apr8.html')
+   
 
-def make_raw_plot():
+def make_raw_plot(pa_files, tceq):
     fig = make_subplots(rows=2, cols=1, shared_xaxes=True,
-                        x_title='Time(CST)',
+                        x_title='Time(CST)', vertical_spacing=0.05,
                         specs=[[{"secondary_y":False}], [{"secondary_y":True}]])
     # fig = go.Figure()
-    pa_files = PAfiles('data\\pa_covid')
+    
     plot_purpleair_pm(fig, pa_files)
-    plot_avg_param(fig, param='Temperature_F')
-    plot_avg_param(fig, param='Humidity_%', second_y=True)
-    plot_avg_pm(fig, param='PM2.5_ATM_ug/m3')
+    
+    # plot_avg_param(fig, param='Temperature_F')
+    # plot_avg_param(fig, param='Humidity_%', second_y=True)
+    frequency = 'H'
+    # PurpleAir PM2.5 Average
+    plot_avg_pm(fig, param='PM2.5_ATM_ug/m3', freq=frequency)
 
-    sample = TCEQfile('data/tceq_pm_mar.csv')
-    fig.add_trace(go.Scattergl(x=sample.time, y=sample.data['PM2.5'],
-                               mode='lines', name='TCEQpm'), row=1, col=1)
-
-    label_plot2(fig)
+    # TCEQ PM2.5 
+    tceq = tceq.resample(frequency)
+    fig.add_trace(go.Scattergl(x=tceq.time, y=tceq.data['PM2.5'],
+                               mode='lines', name='TCEQ PM2.5'), row=1, col=1)
+    
+    # TCEQ Temp & Humidity
+    plot_tceq_trh(fig, freq=frequency)
+    
     label_plot(fig)
+    plot(fig, filename='temp-timeseries-hourly.html')
 
-pa_files = PAfiles('data\\pa_covid')
-sample = TCEQfile('data/tceq_pm_mar.csv')
-make_raw_plot()
+
+pa_files = PAfiles('data/ytd')
+
+tceq = TCEQfile('data/ytd/tceq.csv')
+
+make_raw_plot(pa_files, tceq)
