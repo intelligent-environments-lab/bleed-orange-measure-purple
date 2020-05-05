e68f887e5922eb86d0bd9234308afbf06ca34515
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2020-05-05 10:55:48 -0500

    Added main method

--- sensors/web_scraper.py
+++ sensors/web_scraper.py
@@ -12,13 +12,15 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-browser = webdriver.Chrome()
+from tkinter import *
 
-browser.get('https://www.purpleair.com/sensorlist?exclude=true&nwlat=30.291268505204116&selat=30.272526603783206&nwlng=-97.7717631299262&selng=-97.72423886855452')
+def find_buttons():
+    browser.implicitly_wait(10)
+    buttons = browser.find_elements_by_css_selector('button[id$="_download_button"]')
+    print(buttons)
+    
+    return buttons
 
-browser.implicitly_wait(10)
-buttons = browser.find_elements_by_css_selector('button[id$="_download_button"]')
-print(buttons)
 
 def primary():
     for button in buttons: 
@@ -29,5 +31,20 @@ def primary_A():
     for button in buttons: 
         if 'Primary (A)' in button.text:
             button.click() 
-        
-        
+
+if __name__ == "__main__":
+    browser = webdriver.Chrome()
+    
+    browser.get('https://www.purpleair.com/sensorlist?exclude=true'
+                '&nwlat=30.291268505204116&selat=30.272526603783206'
+                '&nwlng=-97.7717631299262&selng=-97.72423886855452')
+    
+    buttons = find_buttons()
+    
+    root = Tk()
+    root.title('Download Assist')
+    root.geometry('200x150')
+    root.resizable(False, False)
+    btn = Button(root, text='Primary(A)', command=primary_A)
+    btn.pack()
+    root.mainloop()
