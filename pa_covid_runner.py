8ee1498a8be9c72a827d1dc25f961bd64da61cb3
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2020-05-05 10:56:44 -0500

    Moved outlier function to external file

--- pa_covid_runner.py
+++ pa_covid_runner.py
@@ -13,22 +13,9 @@ import numpy as np
 
 from sensors.pa_datafile import PAfiles
 from sensors.TCEQ_pm_datafile import TCEQfile
-
+from analysis.outliers_remover import remove_outlier
 #https://plotly.com/python/getting-started/
 
-def remove_outlier(df, param):
-    # https://stackoverflow.com/questions/34782063/how-to-use-pandas-filter-with-iqr
-    Q1 = df[param].quantile(0.25)
-    Q3 = df[param].quantile(0.75)
-    IQR = Q3-Q1
-    mask = df[param].between(Q1-5*IQR, Q3+5*IQR, inclusive=True)
-    df = df.loc[mask, :].copy()
-
-    Q1 = df[param].rolling(180, center=True).quantile(0.25)
-    Q3 = df[param].rolling(180, center=True).quantile(0.75)
-    IQR = Q3-Q1
-    mask = (df[param] >= Q1-1.5*IQR)&(df[param] <= Q3+1.5*IQR)
-    return df.loc[mask, :]
 
 def plot_purpleair_pm(fig, files, param='PM2.5_ATM_ug/m3'):
     for file in files:
@@ -58,8 +45,11 @@ def plot_avg_param(fig, param='Temperature_F', second_y=False, r=2, c=1):
 def plot_avg_pm(fig, param='PM2.5_ATM_ug/m3', second_y=False, r=1, c=1, freq=None):
     
     #A list of series with PM data
-    combined_data = [remove_outlier(file[:], param).resample(freq).mean()[param].rename(file.sensorname) 
-                     for file in pa_files if file[param] is not None]
+    # combined_data = [remove_outlier(file[:], param).resample(freq).mean()[param].rename(file.sensorname) 
+    #                   for file in pa_files if file[param] is not None]
+    
+    combined_data = [remove_outlier(file[:], param).resample('H').mean().rolling(window=100, min_periods=1, center=True).mean()[param].rename(file.sensorname) 
+                    for file in pa_files if file[param] is not None]
     combined_data =  pd.concat(combined_data, axis=1) #columns = sensors, rows = pm values
     avg = combined_data.mean(axis=1)  #average all sensors
     values = avg
@@ -72,8 +62,9 @@ def plot_tceq_trh(fig, freq=None):
     tceq_trh = pd.read_csv('data/ytd/tceq_trh.csv')
     tceq_trh['Time']=pd.to_datetime(tceq_trh['Time'], format='%Y-%m-%d %H:%M:%S')
     tceq_trh = tceq_trh.set_index('Time')
-    tceq_trh = tceq_trh.resample(freq).mean()
-    
+    # tceq_trh = tceq_trh.resample(freq).mean()
+    tceq_trh = tceq_trh.rolling(window=100, min_periods=1, center=True).mean()
+
     fig.add_trace(go.Scattergl(x=tceq_trh.index, y=tceq_trh['Temperature(F)'],
                                mode='lines', name='TCEQ Temperature'), row=2, col=1, secondary_y=False)
     fig.add_trace(go.Scattergl(x=tceq_trh.index, y=tceq_trh['Relative Humidity(%)'],
@@ -83,7 +74,7 @@ def label_plot(fig):
     fig.update_layout(
         yaxis2=dict(title="Temperature(F)", side='left', anchor='x2'),
         yaxis3=dict(title="Relative humidity(%)", side='right', anchor='x2'),
-        title='UT PurpleAir Hourly Averages',
+        title='UT PurpleAir Rolling Hourly (window=100)',
         yaxis_title='PM 2.5(ug/m3)'
         )
    
@@ -94,17 +85,18 @@ def make_raw_plot(pa_files, tceq):
                         specs=[[{"secondary_y":False}], [{"secondary_y":True}]])
     # fig = go.Figure()
     
-    plot_purpleair_pm(fig, pa_files)
+    # plot_purpleair_pm(fig, pa_files)
     
     # plot_avg_param(fig, param='Temperature_F')
     # plot_avg_param(fig, param='Humidity_%', second_y=True)
-    frequency = 'H'
+    frequency = 'D'
     # PurpleAir PM2.5 Average
     plot_avg_pm(fig, param='PM2.5_ATM_ug/m3', freq=frequency)
 
     # TCEQ PM2.5 
-    tceq = tceq.resample(frequency)
-    fig.add_trace(go.Scattergl(x=tceq.time, y=tceq.data['PM2.5'],
+    tceq_a = tceq.rolling(100)
+    # tceq_a = tceq.resample(frequency)
+    fig.add_trace(go.Scattergl(x=tceq_a.time, y=tceq_a['PM2.5'],
                                mode='lines', name='TCEQ PM2.5'), row=1, col=1)
     
     # TCEQ Temp & Humidity
