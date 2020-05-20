08761ad110d87f3c354986579beaad03bced0296
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2020-05-19 20:52:23 -0500

    Created a timer decorator function

--- sensors/common/util/timer.py
+++ sensors/common/util/timer.py
@@ -8,16 +8,29 @@ Refer to: https://realpython.com/python-timer/
 
 """
 import time
+import functools
 
 class Timer:
+    """A timer class to be used with the 'with' statement"""
     def __init__(self, msg=''):
         self.start = None
         self.msg = msg
-    
+
     def __enter__(self):
         self.start = time.perf_counter()
-    
+
     def __exit__(self, exc_type, exc_value, exc_tb):
         end = time.perf_counter()
         print(f'{self.msg}{float(int((end-self.start)*1000))/1000} secs')
 
+def timeit(func):
+    """Timer decorator: put @timeit before any function to time it in seconds"""
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        value = func(*args, **kwargs)
+        end = time.perf_counter()
+        elapsed = end - start
+        print(f'Elapsed time:{elapsed:0.4f} seconds')
+        return value
+    return wrapper
