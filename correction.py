b27117ce89c0a572ca0a13eed2991f4a8eeec876
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2020-05-25 23:57:14 -0500

    Change to using tidy data

--- correction.py
+++ correction.py
@@ -32,31 +32,33 @@ def plot_avg_pm2(param='PM2.5_ATM_ug/m3', freq=None):
 
     
 if __name__ == "__main__":
+    # %% Import data from csvs
     @Util.caching(cachefile='purpleair.cache')
     def import_PAfiles():
         return PAfiles('data/monthly', keepOutliers=False)
     
     pa_files = import_PAfiles()
     
-    tceq = TCEQfile('data/monthly/tceq.csv')
-    tceq_trh = pd.read_csv('data/monthly/tceq_trh.csv')#.set_index('Time')
+    tceq = pd.read_csv('data/monthly/2020 Webberville-Interstate PM 2.5.csv').set_index('Time')
     
-    tceq_trh['Time'] = pd.to_datetime(tceq_trh['Time'], format='%Y-%m-%d %H:%M:%S')
-    tceq_trh = tceq_trh.set_index('Time').tz_localize('US/Central', nonexistent='shift_forward')
+    tceq_trh = pd.read_csv('data/monthly/2020 Camp Mabry TRH.csv')#.set_index('Time')
     
-    # Create column with the average pm across all sensors
+    tceq_trh['Time'] = pd.to_datetime(tceq_trh['Time'], format='%Y-%m-%d %H:%M:%S %Z')
+    tceq_trh = tceq_trh.set_index('Time').tz_convert('US/Central')
+    
+    # %% Use data from all Purple Airs to create a single average
     pa_avg = plot_avg_pm2(freq='H').rename('PM2.5_ATM_ug/m3')
     pa_avg.index.name = 'Time'
     
     # Combine PA data with tceq trh data in dataframe
     # df = pa_avg.append(tceq_trh).rename(columns={0:'PurplePM2.5'})
-    df = pd.DataFrame(pa_avg).merge(tceq_trh, how='inner', on='Time')
-    df = df.merge(tceq.data.tz_convert('US/Central'), how='inner', on='Time').dropna(how='any')
+    df = pd.DataFrame(pa_avg).merge(tceq_trh, how='outer', left_index=True, right_index=True)
+    df = df.merge(tceq, how='outer', left_index=True, right_index=True).dropna(how='any')
     
     
     pm_vector = df['PM2.5_ATM_ug/m3']
-    X = df[['PM2.5_ATM_ug/m3','Relative Humidity(%)', 'Temperature(F)']] ## X usually means our input variables (or independent variables)
-    y = df['PM2.5'] ## Y usually means our output/dependent variable
+    X = df[['PM2.5_ATM_ug/m3','Relative Humidity (%)', 'Temperature (F)']] ## X usually means our input variables (or independent variables)
+    y = df['PM 2.5 (ug/m3)'] ## Y usually means our output/dependent variable
     X = sm.add_constant(X) ## let's add an intercept (beta_0) to our model
     
     # Note the difference in argument order
@@ -64,14 +66,14 @@ if __name__ == "__main__":
     # predictions = model.predict(X)
     
     # Quadratic regression
-    data = {'temp':df['Temperature(F)'],'r':df['Relative Humidity(%)'],'purple':df['PM2.5_ATM_ug/m3'],'tceq':df['PM2.5']}
+    data = {'temp':df['Temperature (F)'],'r':df['Relative Humidity (%)'],'purple':df['PM2.5_ATM_ug/m3'],'tceq':df['PM 2.5 (ug/m3)']}
     dewpoint = ((data['temp']-32)*5/9+((100-data['r'])/5))*9/5+32
     data['dp']=dewpoint
     # I() is used to get it to do quadratic regression properly
     model = smf.ols(formula='tceq ~ temp + r + purple + I(r**2) + I(temp**2) + I(purple**2) + I(r*temp) + I(purple*temp) + I(r*purple)', data=data).fit()
     # model = smf.ols(formula='tceq ~ temp + r + purple + dp', data=data).fit()
     
-    data1 = {'temp':df['Temperature(F)'],'r':df['Relative Humidity(%)'],'purple':df['PM2.5_ATM_ug/m3']}
+    data1 = {'temp':df['Temperature (F)'],'r':df['Relative Humidity (%)'],'purple':df['PM2.5_ATM_ug/m3']}
     predictions = model.predict(data)
     # Print out the statistics
     print(model.summary())
@@ -109,3 +111,6 @@ if __name__ == "__main__":
     fig.add_trace(go.Scattergl(x=df.index, y=predictions,
                                mode='lines', name='predict'))
     plot(fig)
+    
+    predictions.index = predictions.index.strftime('%Y-%m-%d %H:%M:%S %Z')
+    predictions.to_csv('2020 PurpleAir PM 2.5 corrected.csv')
