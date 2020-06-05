2803cf7d52362099855131b09e559cf4bf0e3f40
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2020-06-05 03:06:38 -0500

    File rename

--- sensors/webtools/tceq_web_parser.py
+++ sensors/webtools/tceq_web_parser.py
@@ -12,18 +12,14 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 import pandas as pd
 import numpy as np
-
-browser = webdriver.Chrome()
-actions = ActionChains(browser)
-
-def get_site():
-    # %%
+ 
+def get_site(browser):
     browser.get('https://www.tceq.texas.gov/cgi-bin/compliance/monops/yearly_summary.pl')
     site_field =  browser.find_element_by_id('find_site_text')
     site_field.send_keys('1068')
     site_field.send_keys(Keys.RETURN)
-    # %%
-def flatten(df):
+
+def flatten(df, data):
     dates = np.array(df.iloc[1:,0])
     hours = np.array(df.iloc[0,1:])
     timestamp = np.array([date+' '+hour for date in dates for hour in hours])
@@ -53,25 +49,39 @@ def replace_strings(pd_series):
     """ Numbers only please """
     return pd.to_numeric(pd_series, errors='coerce')
 
-get_site()
 # %% Start parsing
 def deselect_stats():
     stats = browser.find_elements_by_css_selector('input[name^="print_"')
     for stat in stats:
         stat.click()
-        
-data = browser.find_element_by_css_selector('pre').text
-data = pd.DataFrame([line.split(',') for line in data.split('\n')])
 
-first_column_values = data.iloc[:,0]
-start_indices = data.index[first_column_values=='Date'].values[0]
+def select_format():
+    browser.find_element_by_css_selector('input[value="24hr"]').click()
+    browser.find_element_by_css_selector('input[value="comma"]').click()
+    
+def parse():
+    data = browser.find_element_by_css_selector('pre').text
+    data = pd.DataFrame([line.split(',') for line in data.split('\n')])
+    
+    first_column_values = data.iloc[:,0]
+    start_indices = data.index[first_column_values=='Date'].values[0]
+    
+    d2 = data.iloc[start_indices:, 0:]
+    d2 = flatten(d2, data)
+    d2 = format_time(d2)
+    
+    col_name = d2.columns[1]
+    d2[col_name] = replace_strings(d2[col_name])
+    
+    # %% Save csv
+    d2.to_csv('2020 Camp Mabry RH.csv', index=False)
 
-d2 = data.iloc[start_indices:, 0:]
-d2 = flatten(d2)
-d2 = format_time(d2)
-
-col_name = d2.columns[1]
-d2[col_name] = replace_strings(d2[col_name])
-
-# %% Save csv
-d2.to_csv('2020 Camp Mabry RH.csv', index=False)
+def main():
+    get_site(browser)
+    deselect_stats()
+    select_format()
+    
+if __name__ == '__main__':
+    browser = webdriver.Chrome()
+    actions = ActionChains(browser)
+    main()
