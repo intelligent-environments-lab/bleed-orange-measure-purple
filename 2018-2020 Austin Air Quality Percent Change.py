62c67d957b3a936c65ac263dd70be13220ec65d6
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2020-06-05 03:05:41 -0500

    Add percent change plot

--- 2018-2020 Austin Air Quality Percent Change.py
+++ 2018-2020 Austin Air Quality Percent Change.py
@@ -40,13 +40,19 @@ def process_data(years_of_data, column):
         dataset[column] = to_numeric(dataset[column])
         dataset = shift_year_to_present(dataset)
         years_of_data[year] = set_datetimeindex(dataset)
-
+    
+    
+    
     for year, dataset in years_of_data.items():
-        years_of_data[year] = dataset.rolling(24*7, center=True, min_periods=24*3).mean().resample('3D').mean()
+        years_of_data[year] = dataset.rolling(24*7, center=True, min_periods=24*3).mean().resample('D').mean()
+    
+    baseline = get_baseline([dataset for year, dataset in years_of_data.items() if year != '2020'])
+    percent_diff = get_percent_error(baseline, years_of_data['2020'][column])
+    
+    
+    return years_of_data, percent_diff
 
-    return years_of_data
-
-def create_fig(years_of_data, column, mode='lines', hide_background=True):
+def create_fig(percent_change, column, mode='lines', hide_background=True):
     """ Drawing the lines and stuff """
     if hide_background:
         layout = go.Layout(
@@ -57,25 +63,21 @@ def create_fig(years_of_data, column, mode='lines', hide_background=True):
     else:
         fig = go.Figure()
 
-    for year, dataset in years_of_data.items():
-        if year == '2020':
-            opacity = 1
-        else:
-            opacity = 0.5
+
             
-        # Only use data from Feb 1-May 1
-        dataset = dataset[dataset.index >= '2020-02-01 00:00:00-06:00']
-        dataset = dataset[dataset.index < '2020-05-01 00:00:00-06:00']
-        
-        fig.add_trace(go.Scattergl(x=dataset.index, y=dataset[column],
-                                   mode=mode, name=year, opacity=opacity))
+    # Only use data from Feb 1-May 1
+    percent_change = percent_change[percent_change.index >= '2020-02-01 00:00:00-06:00']
+    percent_change = percent_change[percent_change.index < '2020-05-01 00:00:00-06:00']
+    
+    fig.add_trace(go.Scattergl(x=percent_change.index, y=percent_change,
+                               mode=mode, name='Percent Change', opacity=1))
 
     fig.update_layout(xaxis=dict(tickformat="%b",
                                  tick0="2020-01-02",
                                  dtick=30.42 * 24 * 60 * 60 * 1000))
 
     fig.update_xaxes(range=["2020-02-01", "2020-05-03"])
-    fig.update_yaxes(range=[0, max(dataset[column])+2])
+    fig.update_yaxes(range=[min(percent_change)-2, max(percent_change)+2])
     return fig
 
 def label_plot(fig, title, xtitle, ytitle):
@@ -110,6 +112,11 @@ def highlight_covid(fig):
             ]
         )
 
+def get_baseline(datasets):
+    return pd.concat(datasets, axis=1).mean(axis=1)
+
+def get_percent_error(base, actual):
+    return (actual-base)/base*100
 # =============================================================================
 #  Functions to import and plot the data 
 # =============================================================================
@@ -118,79 +125,82 @@ def highlight_covid(fig):
 def ozone_plot(root):
     column = 'Ozone (ppb)'
 
-    @Util.caching(cachefile='.cache/18-20ozone.cache')
+    # @Util.caching(cachefile='.cache/18-20ozone.cache')
     def _import():
         return process_data({'2020': pd.read_csv(f'{root}/2020 Edwards ozone.csv'),
                              '2019': pd.read_csv(f'{root}/2019 Edwards ozone.csv'),
                              '2018': pd.read_csv(f'{root}/2018 Edwards ozone.csv')},
                             column)
-    ozone = _import()
-    fig = create_fig(ozone, column)
+    ozone, percent_diff = _import()
+    percent_diff = percent_diff.rename('error')
+    fig = create_fig(percent_diff, column)
     highlight_covid(fig)
     label_plot(fig, 'Ozone Levels in Austin 2018-2020',
                'Month of the Year',
                'Ozone (parts per billion)')
 
-    fig.write_image("2018-2020 Austin Ozone.png", scale=1.5)
+    fig.write_image("2018-2020 Austin Ozone percent change.png", scale=1.5)
 
 # %% Oxides of Nitrogen
 def NOx_plot(root):
     column = 'NOx (ppb)'
 
-    @Util.caching(cachefile='.cache/18-20NOx.cache')
+    # @Util.caching(cachefile='.cache/18-20NOx.cache')
     def _import():
         return process_data({'2020': pd.read_csv(f'{root}/2020 Interstate.csv'),
                              '2019': pd.read_csv(f'{root}/2019 Interstate.csv'),
                              '2018': pd.read_csv(f'{root}/2018 Interstate.csv')},
                             column)
-    NOx = _import()
-    fig = create_fig(NOx, column)
+    NOx, percent_diff = _import()
+    percent_diff = percent_diff.rename('error')
+    fig = create_fig(percent_diff, column)
     highlight_covid(fig)
     label_plot(fig, 'NOx Levels in Austin 2018-2020',
                'Month of the Year',
                'NOx (parts per billion)')
 
-    fig.write_image("2018-2020 Austin NOx.png", scale=1.5)
+    fig.write_image("2018-2020 Austin NOx percent change.png", scale=1.5)
 
 # %% Nitrogen Dioxide data
 def NO2_plot(root):
     column = 'NO2 (ppb)'
 
-    @Util.caching(cachefile='.cache/18-20NO2.cache')
+    # @Util.caching(cachefile='.cache/18-20NO2.cache')
     def _import():
         return  process_data({'2020': pd.read_csv(f'{root}/2020 Interstate.csv'),
                               '2019': pd.read_csv(f'{root}/2019 Interstate.csv'),
                               '2018': pd.read_csv(f'{root}/2018 Interstate.csv')},
                              column)
-    NO2 = _import()
-    fig = create_fig(NO2, column)
+    NO2, percent_diff = _import()
+    percent_diff = percent_diff.rename('error')
+    fig = create_fig(percent_diff, column)
     highlight_covid(fig)
     label_plot(fig, 'NO2 Levels in Austin 2018-2020',
                'Month of the Year',
                'NO2 (parts per billion)')
 
-    fig.write_image("2018-2020 Austin NO2.png", scale=1.5)
+    fig.write_image("2018-2020 Austin NO2 percent change.png", scale=1.5)
 
 # %% Particulate Matter 2.5 data
 def PM_plot(root):
     """ Plots averaged PM2.5 data from TCEQ Webberville and Interstate sensors"""
     column = 'PM 2.5 (ug/m3)'
 
-    @Util.caching(cachefile='.cache/18-20PM.cache')
+    # @Util.caching(cachefile='.cache/18-20PM.cache')
     def _import():
         return process_data({'2020': pd.read_csv(f'{root}/2020 Webberville-Interstate PM 2.5.csv'),
                              '2019': pd.read_csv(f'{root}/2019 Webberville-Interstate PM 2.5.csv'),
                              '2018': pd.read_csv(f'{root}/2018 Webberville-Interstate PM 2.5.csv')},
                             column)
-    PM = _import()
-
-    fig = create_fig(PM, column)
+    pm, percent_diff = _import()
+    percent_diff = percent_diff.rename('error')
+    fig = create_fig(percent_diff, column)
     highlight_covid(fig)
     label_plot(fig, 'PM 2.5 Levels in Austin 2018-2020',
                'Month of the Year',
                'PM 2.5 (ug/m3)')
 
-    fig.write_image("2018-2020 Austin PM 2.5.png", scale=1.5)
+    fig.write_image("2018-2020 Austin PM 2.5 percent change.png", scale=1.5)
 
 # %% Function calls
 def main():
