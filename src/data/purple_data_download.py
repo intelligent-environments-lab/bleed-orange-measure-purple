29ccce47bfa83282e60dd4e491469cf5cd829b41
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2020-06-16 01:24:53 -0500

    Add filename generation, replace urlib with requests

--- src/data/purple_data_download.py
+++ src/data/purple_data_download.py
@@ -6,12 +6,13 @@ Created on Sat Jun 13 01:56:30 2020
 """
 
 from urllib.request import urlopen
+import requests
 import json
 
 import pandas as pd
 
-def load_keys():
-    with open('thingspeak_keys.json', 'r', encoding='utf8') as file:
+def import_json(filename):
+    with open(filename, 'r', encoding='utf8') as file:
         filedata = json.load(file)
     return filedata
 
@@ -32,15 +33,47 @@ def get_key(keys, sensor_name, mode='primaryA'):
     return channel_ID, api_key
     
 
-def send_url_request(channel, key, start=None, end=None, file_format='csv'):
+def send_url_request(channel, api_key, start=None, end=None):
     start_date = start.strftime('%Y-%m-%d')
     end_date = end.strftime('%Y-%m-%d')
-    url = f'https://api.thingspeak.com/channels/{channel}/feeds.{file_format}?api_key={key}&start={start_date}%2000:00:00&end={end_date}%2000:00:00'
-    response = urlopen(url)
-    return response.read()
+    url = f'https://api.thingspeak.com/channels/{channel}/feeds.csv?api_key={api_key}&start={start_date}%2000:00:00&end={end_date}%2000:00:00'
+    return requests.get(url).text
 
 
-def run_request(start_date, end_date, mode='primaryA', keys=None):
+def generate_filename(metadata_sets, sensor_name, start_date, end_date, mode):
+    metadata = metadata_sets[sensor_name]
+    
+    location_type = metadata['DEVICE_LOCATIONTYPE']
+    lat = metadata['Lat']
+    lon = metadata['Lon']
+
+    if 'primary' in mode:
+        datatype = 'Primary Real Time'
+    else:
+        datatype = 'Secondary Real Time'
+        
+    start_date = start_date.strftime('%m_%d_%Y')
+    end_date = end_date.strftime('%m_%d_%Y')
+
+    filename = f'{sensor_name} ({location_type}) ({lat} {lon}) {datatype} {start_date} {end_date}.csv'
+    return filename
+
+      
+def combine_and_export(datasets, sensor_name, metadata, filename):
+    columns = ['entry_id','PM1.0_CF1_ug/m3','PM2.5_CF1_ug/m3','PM10.0_CF1_ug/m3','UptimeMinutes','RSSI_dbm','Temperature_F','Humidity_%','PM2.5_ATM_ug/m3']
+    if type(datasets[0]) is bytes:
+        datasets = [data.decode('utf8') for data in datasets]
+    frames = [pd.DataFrame([line.split(',') for line in data.split('\n')])
+              for data in datasets]
+    frames = [frame.drop(frame.index[0]).set_index(0).dropna(how='all') for frame in frames]
+    single_file = pd.concat(frames)
+    single_file.index.name = 'created_at'
+    single_file.columns = columns
+    single_file.to_csv(filename)
+
+
+
+def run_request(start_date, end_date, mode='primaryA', keys=None, save_location=None):
     start_date = pd.to_datetime(start_date, format='%Y-%m-%d', infer_datetime_format=True)
     end_date = pd.to_datetime(end_date, format='%Y-%m-%d', infer_datetime_format=True) + pd.Timedelta('1d')
     delta = pd.Timedelta('11d')
@@ -59,23 +92,14 @@ def run_request(start_date, end_date, mode='primaryA', keys=None):
                 file_end = end_date
             if file_start >= end_date or file_start >= file_end:
                 break
-        combine_and_export(sensor_data, name, metadata)
+        filename = generate_filename(keys, name, start_date, end_date, mode)
+        if save_location is not None:
+            filename = save_location + '/' + filename
+        combine_and_export(sensor_data, name, metadata, filename)
         
-def combine_and_export(datasets, name, metadata):
-    columns = ['entry_id','PM1.0_CF1_ug/m3','PM2.5_CF1_ug/m3','PM10.0_CF1_ug/m3','UptimeMinutes','RSSI_dbm','Temperature_F','Humidity_%','PM2.5_ATM_ug/m3']
-    if type(datasets[0]) is bytes:
-        datasets = [data.decode('utf8') for data in datasets]
-        frames = [pd.DataFrame([line.split(',') for line in data.split('\n')])
-                  for data in datasets]
-        frames = [frame.drop(frame.index[0]).set_index(0).dropna(how='all') for frame in frames]
-    single_file = pd.concat(frames)
-    single_file.index.name = 'created_at'
-    single_file.columns = columns
-    single_file.to_csv('ca.csv')
-    
 def main():
-    thingkeys = load_keys()
-    run_request('2020-1-1', '2020-6-1', keys=thingkeys)
+    thingkeys = import_json('src/data/thingspeak_keys.json')
+    run_request('2020-1-1', '2020-6-1', keys=thingkeys, save_location='data/raw/test')
     
 if __name__ == '__main__':
     main()
