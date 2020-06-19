9ce802ff43a61d4d63b78cd9eb83ac8ccc693e84
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2020-06-19 13:16:27 -0500

    Add some docstrings

--- src/data/make_tceq_dataset.py
+++ src/data/make_tceq_dataset.py
@@ -8,19 +8,37 @@ Created on Tue Jun 16 11:22:43 2020
 import requests
 from bs4 import BeautifulSoup
 
-def create_form(cams_site, year, param):
-    """ Creates a form to send in a POST request to TCEQ website"""
-    
+def create_form(site, param, year):
+    """Creates a form to send in a POST request to TCEQ website
+
+
+    Parameters
+    ----------
+    site : int
+        CAMS code for the site.
+    param : int
+        5-digit AQS air quality parameter code.
+    year : int
+        4-digit year.
+
+    Returns
+    -------
+    request : dict
+        Can be sent in POST requests.
+
+    """
+
+
     request = {'submitted':'1',
-               'select_site':f'site|||{cams_site}',
+               'select_site':f'site|||{site}',
                'user_year':f'{year}',
                'user_param':f'{param}',
                'time_format':'24hr',
                'report_format':'comma'}
-    
+
     return request
 
-def create_forms(years, sites):
+def create_forms(years, sites=None):
     """ Creates multiple forms for the provided years, sites, and parameters"""
     forms = []
     for year in years:
@@ -30,15 +48,43 @@ def create_forms(years, sites):
     return forms
 
 def extract_data_from_html(html):
+    """Extracts the csv data table from the html page source
+
+
+    Parameters
+    ----------
+    html : str
+        Page source code in its entirety.
+
+    Returns
+    -------
+    str
+        Raw unformatted csv data including the metadata rows.
+
+    """
     soup = BeautifulSoup(html, 'html.parser')
     data = soup.find('pre')
     return data.text
 
 def export_to_file(data, filename):
-    """ Writes data string to file """
+    """Produces a csv file that contains the provided data
+
+
+    Parameters
+    ----------
+    data : str
+        Data string to be written to file.
+    filename : str
+        Name for the output file, can also include relative path from working directory.
+
+    Returns
+    -------
+    None.
+
+    """
     with open(filename, 'w', encoding='utf8') as file:
         file.write(data)
-        
+
 def main():
     """ Entry point for the program """
     request_url = 'https://www.tceq.texas.gov/cgi-bin/compliance/monops/yearly_summary.pl'
@@ -50,11 +96,11 @@ def main():
              'Interstate':{'cams': 1068,
                            'params':[42601, 42602, 88101]}
              }
-    request = create_form('0171', '2020', '88101')
+    request = create_form('0171', '88101', '2020')
     res = create_forms(years, sites)
     response = requests.post(request_url, data=request)
     data = extract_data_from_html(response.text)
     print(response.text)
-    
+
 if __name__ == '__main__':
     main()
