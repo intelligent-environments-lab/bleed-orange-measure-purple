4d3fdb38426a8882c7dc87a45a789f1a977528e3
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2020-06-22 16:39:03 -0500

    Update downloader scripts

--- src/data/make_tceq_dataset.py
+++ src/data/make_tceq_dataset.py
@@ -38,13 +38,17 @@ def create_form(site, param, year):
 
     return request
 
-def create_forms(years, sites=None):
+def create_forms(sites):
     """ Creates multiple forms for the provided years, sites, and parameters"""
+
     forms = []
-    for year in years:
-        for _, site in sites.items():
-            for param in site['params']:
-                forms.append(create_form(site['cams'], year, param))
+    for site in sites:
+        params = site['params']
+        for param in params:
+            years = site['years']
+            for year in years:
+                cams_code = site['cams']
+                forms.append(create_form(cams_code, param, year))
     return forms
 
 def extract_data_from_html(html):
@@ -64,9 +68,18 @@ def extract_data_from_html(html):
     """
     soup = BeautifulSoup(html, 'html.parser')
     data = soup.find('pre')
-    return data.text
+    if data is None:
+        return None
+    else:
+        return data.text
 
-def export_to_file(data, filename):
+def extract_data_from_htmls(htmls):
+
+    datasets = [extract_data_from_html(html) for html in htmls
+                if extract_data_from_html(html) is not None]
+    return datasets
+
+def export_to_file(data, filename='temp_file.csv'):
     """Produces a csv file that contains the provided data
 
 
@@ -85,22 +98,34 @@ def export_to_file(data, filename):
     with open(filename, 'w', encoding='utf8') as file:
         file.write(data)
 
-def main():
+def post_requests(url, forms):
+    print(f"Posting {len(forms)} requests...")
+    responses = [requests.post(url, data=form).text for form in forms]
+    print("Posting complete")
+    return responses
+
+
+def main(site, param=None, years=None, save_location=None):
     """ Entry point for the program """
     request_url = 'https://www.tceq.texas.gov/cgi-bin/compliance/monops/yearly_summary.pl'
-    years = [2020, 2019, 2018, 2017, 2016]
-    sites = {'Webberville':{'cams':171,
-                            'params':[88101]},
-             'Edwards':{'cams': 1605,
-                        'params': [44201]},
-             'Interstate':{'cams': 1068,
-                           'params':[42601, 42602, 88101]}
-             }
-    request = create_form('0171', '88101', '2020')
-    res = create_forms(years, sites)
-    response = requests.post(request_url, data=request)
-    data = extract_data_from_html(response.text)
-    print(response.text)
+
+    # request = create_form(site, param, years)
+    forms = create_forms(site)
+    responses = post_requests(request_url, forms)
+    datasets = extract_data_from_htmls(responses)
+
+    for dataset in datasets:
+        filename = dataset.partition("\n")[0]+'.csv'
+        if save_location is not None:
+            filename = f'{save_location}/{filename}'
+        export_to_file(dataset, filename)
+    print()
+    # response = requests.post(request_url, data=request)
+    # data = extract_data_from_html(response.text)
 
 if __name__ == '__main__':
-    main()
+
+    sites = [{'cams':171, 'params':[88101], 'years':[2020, 2019, 2018, 2017, 2016]},
+               {'cams': 1605, 'params': [44201], 'years':[2020, 2019, 2018, 2017, 2016]},
+              {'cams': 1068, 'params':[42601, 42602, 88101], 'years':[2020, 2019, 2018, 2017, 2016]}]
+    main(sites, save_location='data/raw/tceq')
