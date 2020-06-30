c3fdf036d11baf40944a16fa6f206b1e74639a57
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2020-06-30 14:04:42 -0500

    Create 2015-2020 Mean concentration plot Ignore .vscode

--- src/visualization/2018-2020 Austin Air Quality Mean Conc subplots.py
+++ src/visualization/2018-2020 Austin Air Quality Mean Conc subplots.py
@@ -189,18 +189,36 @@ def run_param_combined(
     fig.write_image(out_file, scale=1.5)
 
 
-def add_subplot_trace(fig, percent_change, column, name=None, num=1):
+def add_subplot_trace(fig, percent_change, column, name=None, num=1, showlegend=True):
     if name is None:
         name = column
     percent_change = percent_change[percent_change.index >= '2020-02-01 00:00:00-06:00']
     percent_change = percent_change[percent_change.index < '2020-05-01 00:00:00-06:00']
 
+    opacity = 1
+    width = 3
+    if name != 2020:
+        opacity = 0.6
+        width = 2
+
+    color = {2020:'#636EFA',
+             2019:'#ef553b',
+             2018:'#00cc96',
+             2017:'#ab63fa',
+             2016:'#ffa15a',
+             2015:'#19d3f3'
+             }
+    showlegend=False
+    if num==2:
+        showlegend=True
     fig.add_trace(
-        go.Scattergl(x=percent_change.index, y=percent_change, mode='lines', name=name),
+        go.Scattergl(x=percent_change.index, y=percent_change, mode='lines', name=name, legendgroup=name, showlegend=showlegend,
+                     line=dict(width=width, color=color[name]),
+
+                     opacity=opacity),
         row=num,
         col=1,
     )
-
 
 def run_param_combined_subplots(
     param_files=None,
@@ -281,6 +299,94 @@ def run_param_combined_subplots(
 
     fig.write_image(out_file, scale=1.5)
 
+def run_param_combined_subplots_mean(
+    param_files=None,
+    root='',
+    title='2015-2020 Air Quality Parameters',
+    xtitle='Month of the Year',
+    ytitle='Percent Change',
+    out_file='temp_plot.png',
+):
+    fig = make_subplots(
+        rows=3,
+        cols=1,
+        shared_xaxes=True,
+        x_title='Month of the Year',
+        # y_title='Mean Concentr(%)',
+        vertical_spacing=0.0,
+    )
+    years_in_legend_tracker = []
+    count = 1
+    for param_name, filename in param_files.items():
+        data = pd.read_feather(f'{root}/{filename}').set_index('Time')
+        years = [key for key in data.groupby(data.index.year).groups.keys()]
+        dataset = {year: data[data.index.year == year] for year in years}
+        year_min = [key for key in dataset.keys()][0]
+        dataset, percent_diff = process_data(dataset, dataset[year_min].columns[0])
+        # percent_diff = percent_diff.rename('error')
+        for year, dataset in dataset.items():
+            showlegend=True
+            if year in years_in_legend_tracker:
+                showlegend=False
+            add_subplot_trace(fig, dataset.iloc[:,0], 'lines', name=year, num=count, showlegend=showlegend)
+            years_in_legend_tracker.append(year)
+        # add_subplot_trace(fig, percent_diff, 'lines', name=param_name, num=count)
+        count += 1
+
+    fig.write_image(out_file, scale=1.5)
+    # Hides the background and grid
+    fig.update_layout(
+        plot_bgcolor='rgba(0,0,0,0)',
+        xaxis={
+            'showgrid': False,
+            'showline': True,
+            'linecolor': 'black',
+        },  # , 'zeroline':False, 'linewidth': 0.01
+        xaxis2={'showgrid': False, 'showline': True, 'linecolor': 'black'},
+        xaxis3={'showgrid': False, 'showline': True, 'linecolor': 'black'},
+        yaxis={
+            'showgrid': False,
+            'showline': True,
+            'linecolor': 'black',
+            'zeroline': True,
+            'zerolinecolor': 'black',
+            'zerolinewidth': 0.5,
+        },
+        yaxis2={
+            'showgrid': False,
+            'showline': True,
+            'linecolor': 'black',
+            'zeroline': True,
+            'zerolinecolor': 'black',
+            'zerolinewidth': 0.5,
+        },
+        yaxis3={
+            'showgrid': False,
+            'showline': True,
+            'linecolor': 'black',
+            'zeroline': True,
+            'zerolinecolor': 'black',
+            'zerolinewidth': 0.5,
+        },
+    )
+
+    fig.update_yaxes(range=[0, 45], row=1, col=1)
+    fig.update_yaxes(range=[0, 30], row=2, col=1)
+    fig.update_yaxes(range=[0, 20], row=3, col=1)
+
+    highlight_covid(fig)
+
+    fig.update_layout(title=title, xaxis_tickformat='%b')
+    fig.update_yaxes(title_text='Ozone (ppb)', row=1, col=1)
+    fig.update_yaxes(title_text='NO2 (ppb)', row=2, col=1)
+    fig.update_yaxes(title_text='PM 2.5 (ug/m3)', row=3, col=1)
+    fig.update_xaxes(
+        dict(tickformat="%b", tick0="2020-01-02", dtick=30.42 * 24 * 60 * 60 * 1000)
+    )
+
+    fig.write_html(out_file+'.html')
+
+    fig.write_image(out_file, scale=1.5)
 
 # %% Function calls
 def main():
@@ -304,14 +410,14 @@ def main():
     #     out_file="a2018-2020 Austin NO2 percent change.png",
     # )
 
-    run_param_combined_subplots(
+    run_param_combined_subplots_mean(
         param_files={
             'Ozone': 'CAMS 1605 Ozone.feather',
             'NO2': 'CAMS 1068 Nitrogen Dioxide.feather',
             'PM 2.5': 'CAMS 171_1068 PM-2.5.feather',
         },
         root=root,
-        out_file="Austin Air Quality Percent Change from 2015 to 2020 subplot.png",
+        out_file="Austin Air Quality Mean Con from 2015 to 2020 subplot.png",
     )
 
 
