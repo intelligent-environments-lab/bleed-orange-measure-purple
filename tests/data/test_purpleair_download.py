ed295efaf57c96a65c00b5fbd6dacb5137df38dc
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2020-07-06 22:34:59 -0500

    file rename

--- tests/data/test_purpleair_download.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 15 03:19:22 2020
-
-@author: CalvinL2
-"""
-
-import pytest
---
+++ tests/test_requests.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  1 23:29:10 2020
+
+@author: CalvinL2
+"""
+
+from src.data.helpers.requests import AsyncRequest as areq
+from src.data.helpers.requests import StandardRequest as req
+
+
+def test_async_requests():
+    urls = ['https://google.com', 'https://youtube.com']
+    resp = areq.get_urls(urls)
+
+def test_standard_requests():
+    urls = ['https://google.com', 'https://youtube.com']
+    resp = req.get_urls(urls)
+
+def test_tceq_post_async():
+    url = 'https://www.tceq.texas.gov/cgi-bin/compliance/monops/yearly_summary.pl'
+    request = {'submitted':'1',
+               'select_site':'site|||171',
+               'user_year':'2020',
+               'user_param':'88101',
+               'time_format':'24hr',
+               'report_format':'comma'}
+    resp = areq.post_url(url, [request])
+    assert 'Summary Report for 2020' in resp[0]
+
+def test_tceq_post():
+    url = 'https://www.tceq.texas.gov/cgi-bin/compliance/monops/yearly_summary.pl'
+    request = {'submitted':'1',
+               'select_site':'site|||171',
+               'user_year':'2020',
+               'user_param':'88101',
+               'time_format':'24hr',
+               'report_format':'comma'}
+    resp = req.post_url(url, [request])
+    assert 'Summary Report for 2020' in resp[0]
