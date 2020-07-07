08f403e5ef1d5e167adbcdc8f6912cd84b5aa090
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2020-07-07 10:51:55 -0500

    Create ut_weather_raw_cleaner.py

---
+++ src/data/ut_weather_raw_cleaner.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  7 10:01:59 2020
+
+@author: CalvinL2
+"""
+
+import pandas as pd
+
+def main():
+    data =  pd.read_csv('data/external/ut_weather/UTPD Wx Station 5 min data/utweather_5min_12192018-06032020.csv')
+    pass
+if __name__=='__main__':
+    main()
