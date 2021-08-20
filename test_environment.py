bf8e5278e47b2c54e21ca4e287fc07501c199f68
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2021-08-20 08:21:52 -0500

    Fix import reference

--- test_environment.py
+++ test_environment.py
@@ -10,8 +10,8 @@ def test_build():
     # pdr.main(start='2020-1-1', end='2020-9-15', channel='primaryA', save_location='data/raw/purpleair')
     # pdr.main(start='2020-1-1', end='2020-9-15', channel='primaryB', save_location='data/raw/purpleair/B')
     
-    import src.pathname_index as pni
-    import src.data.purpleair_raw_cleaner as pc
+    import bomp.pathname_index as pni
+    import bomp.data.purpleair_raw_cleaner as pc
 
     pc.main(path=pni.pa_raw, save_location=pni.pa_int_real)
     pc.main(path=pni.pa_rawB, save_location=pni.pa_intB_real)
